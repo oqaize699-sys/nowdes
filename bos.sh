111cee40e8daa3c77eec7d33e24114286b7e6251
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-09-10 17:49:19 +0700

    Update bos.sh

--- bos.sh
+++ bos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 2 suto-01 >/dev/null 2>&1 &
+cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 2 suto-01
 sleep 100
 while true
 do
